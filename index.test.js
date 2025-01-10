4e159d536206056e7f03d138d8363235e9cbb07c
timgl <tim@posthog.com>
2025-01-09 18:37:23 -0800

    add all costs and write plugin

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { createEvent, createIdentify, getMeta, resetMeta, clone } = require("@posthog/plugin-scaffold/test/utils");
+const { createEvent, getMeta, resetMeta, clone } = require("@posthog/plugin-scaffold/test/utils");
 
 const { processEvent } = require("./index");
 
@@ -13,7 +13,13 @@ beforeEach(() => {
 
 test("processEvent adds properties", async () => {
     // Create a random event
-    const event0 = createEvent({ event: "booking completed", properties: { amount: "20", currency: "USD" } });
+    const event0 = createEvent({ event: "$ai_generation", properties: {
+        $ai_provider: "openai",
+        $ai_model: "gpt-4-turbo-2024-04-09",
+        $ai_input_tokens: 100,
+        $ai_output_tokens: 200,
+    }
+    });
 
     // Must clone the event since `processEvent` will mutate it
     const event1 = await processEvent(clone(event0), getMeta());
@@ -21,41 +27,10 @@ test("processEvent adds properties", async () => {
         ...event0,
         properties: {
             ...event0.properties,
-            greeting: "Dzień dobry!",
-            greeting_counter: 0,
-            random_number: 4,
+            $ai_input_cost_usd: 0.001,
+            $ai_output_cost_usd: 0.006,
+            $ai_total_cost_usd: 0.007
         },
     });
 
-    const event2 = await processEvent(clone(event0), getMeta());
-    expect(event2).toEqual({
-        ...event1,
-        properties: {
-            ...event1.properties,
-            greeting_counter: 1,
-        },
-    });
-
-    const event3 = await processEvent(clone(event0), getMeta());
-    expect(event3).toEqual({
-        ...event2,
-        properties: {
-            ...event2.properties,
-            greeting_counter: 2,
-        },
-    });
-});
-
-test("processEvent does not crash with identify", async () => {
-    const defaultHelloWorldProperties = Object.freeze({
-        greeting: "Dzień dobry!",
-        greeting_counter: 0,
-        random_number: 4,
-    });
-    // Create a random event
-    const event0 = createIdentify();
-    // Must clone the event since `processEvent` will mutate it
-    const { properties, ...restOfEvent1 } = await processEvent(clone(event0), getMeta());
-    expect(restOfEvent1).toEqual(event0);
-    expect(properties).toEqual(defaultHelloWorldProperties);
 });
